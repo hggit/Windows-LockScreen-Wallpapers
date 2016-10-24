f9915c218e7013f48c4b7019ea4d6515698e447c
Himanshu Gupta <hg24091996@gmail.com>
2016-10-25 01:02:46 +0530

    Update x.cpp

--- x.cpp
+++ x.cpp
@@ -7,21 +7,21 @@
 using namespace std;
 int main()
 {
-    string source="C:/Users/Himanshu/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets";
-    string destination="X:/Himanshu/Pictures/Wallpapers/Windows";
+    string source="C:/Users/Himanshu/AppData/Local/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets";	//verify your source path
+    string destination="X:/Himanshu/Pictures/Wallpapers/Windows";		//set prefered location to keep wallpapers
     
     char rbc[166];
     string s="robocopy "+source+" "+destination;
     strcpy(rbc, s.c_str());
-    system(rbc);
+    system(rbc);		//copy all files
 
-	char cd[42];
-    string t="cd X:/Himanshu/Pictures/Wallpapers/Windows";
+    char cd[42];
+    string t="cd "+destination;
     strcpy(cd, t.c_str());
-    system(cd);
+    system(cd);		//change directory
 
 
-    FILE *fpip;
+    FILE *fpip;		//file to store filenames and sizes
     char *com = "for %k in (*.*) do @echo %~znk";
     char x = 0;
     string files;
@@ -45,7 +45,7 @@ int main()
     		char del[100];
     		string d="del /f /q "+name2;
     		strcpy(del, d.c_str());
-    		system(del);
+    		system(del);		//delete extras
     	}
     	name1=name2;
     } while(iss);
@@ -54,12 +54,12 @@ int main()
     char ren[100];
     string r="ren *. *.jpg";
     strcpy(ren, r.c_str());
-    system(ren);
+    system(ren);		//rename as image
 
     char del[100];
     string d="del /f /q *.";
     strcpy(del, d.c_str());
-    system(del);
+    system(del);		//delete duplicates
 
     
 }
